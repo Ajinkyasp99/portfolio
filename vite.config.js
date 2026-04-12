48cbbad9670e878793e81aa0390d342d341431ff
Ajinkya <ajinkya@carbonmius.com>
2026-04-12 14:11:21 +0530

    [closes #00]: portfolio added

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // Base path for GitHub Pages
+  // Replace 'portfolio' with your repository name
+  base: '/portfolio/',
+});
